159ba5bf094e03563586105f9f4fb6ca7c23c9d8
Sangamesh_Math <91306677+sangamesh-Math@users.noreply.github.com>
2023-07-24 17:29:51 +0530

    Update index.js
    Added a new feature using which users are now able to give inputs from keystrokes.

--- index.js
+++ index.js
@@ -31,5 +31,47 @@ for(var i = 0; i < len; i++)
         }
 
     });
+document.addEventListener('keydown', (event) => {
+        if(event.key == "a") 
+        {
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+        }
+        else if(event.key == "s")
+        {
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+        }
+        else if(event.key == "d")
+        {
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+
+        }
+        else if(event.key == "f")
+        {
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+        }
+        else if(event.key == "j")
+        {
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+        }
+        else if(event.key == "k")
+        {
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+        }
+        else if(event.key == "l")
+        {
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+        }
+        
+    });
+
+    var audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
 
     
